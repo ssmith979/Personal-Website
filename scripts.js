403ef906a5cc13f071bfc43517e1d232f6b6e4fa
Susanna Smith <50603908+ssmith979@users.noreply.github.com>
2020-10-26 14:04:12 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,83 @@
+function HoverOver1(){
+    document.getElementById('bazazz-img1').style.opacity = ".5";
+
+}
+
+function HoverOut1(){
+    document.getElementById('bazazz-img1').style.opacity = ".9";
+
+}
+
+function HoverOver2(){
+    document.getElementById('bazazz-img2').style.opacity = ".5";
+
+}
+
+function HoverOut2(){
+    document.getElementById('bazazz-img2').style.opacity = ".9";
+
+}
+
+/* icons */
+
+function HoverOverGit(){
+    document.getElementById('code').style.opacity = ".5";
+
+}
+
+function HoverOutGit(){
+    document.getElementById('code').style.opacity = ".9";
+
+}
+
+function HoverOverLink(){
+    document.getElementById('link').style.opacity = ".5";
+
+}
+
+function HoverOutLink(){
+    document.getElementById('link').style.opacity = ".9";
+
+}
+
+function HoverOverResume(){
+    document.getElementById('resume').style.opacity = ".5";
+
+}
+
+function HoverOutResume(){
+    document.getElementById('resume').style.opacity = ".9";
+
+}
+
+function HoverOverResume2(){
+    document.getElementById('resume2').style.opacity = ".5";
+    document.getElementById('resume2').style.borderBottom = " white 1pt solid";
+
+}
+
+function HoverOutResume2(){
+    document.getElementById('resume2').style.opacity = ".9";
+    document.getElementById('resume2').style.borderBottom = "none";
+
+}
+
+function HoverOvercert1(){
+    document.getElementById('cert1').style.opacity = ".5";
+
+}
+
+function HoverOutcert1(){
+    document.getElementById('cert1').style.opacity = ".9";
+
+}
+
+function HoverOvercert2(){
+    document.getElementById('cert2').style.opacity = ".5";
+
+}
+
+function HoverOutcert2(){
+    document.getElementById('cert2').style.opacity = ".9";
+
+}
